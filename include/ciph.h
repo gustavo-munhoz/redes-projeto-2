de1c77fa9f74de3d4bbaf5d21e7f670ef2fc69cb
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-17 17:21:57 -0300

    Added new codes for messages

--- include/ciph.h
+++ include/ciph.h
@@ -5,11 +5,15 @@
 #include <string.h>
 
 #define AUTH_REQUEST "AUTH_REQUEST"
-#define AUTH_RESPONSE_OK "AUTH_OK"
-#define AUTH_RESPONSE_ERROR "AUTH_ERROR"
+#define AUTH_OK "AUTH_OK"
+#define AUTH_ERROR "AUTH_ERROR"
+#define NOT_AUTHENTICATED "NOT_AUTHENTICATED"
 #define ENCRYPT_REQUEST "ENCRYPT"
+#define ENCRYPTED_TEXT "ENCRYPTED_TEXT"
+#define ENCRYPT_ERROR "ENCRYPT_ERROR"
 #define LOGOUT_REQUEST "LOGOUT"
-#define LOGOUT_RESPONSE "LOGOUT_OK"
+#define LOGOUT_OK "LOGOUT_OK"
+#define UNKNOWN_COMMAND "UNKNOWN_COMMAND"
 
 void create_auth_request(char *buffer, const char *id, const char *password);
 void create_encrypt_request(char *buffer, const char *text, int shift);
